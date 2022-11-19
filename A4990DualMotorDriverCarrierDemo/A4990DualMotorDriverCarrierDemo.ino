daacae76236542fab4d3046c7d79f48637175ea0
Hubert Liu <52901645+yuxinliuhubert@users.noreply.github.com>
2022-11-18 17:09:41 -0800

    update

--- A4990DualMotorDriverCarrierDemo/A4990DualMotorDriverCarrierDemo.ino
+++ A4990DualMotorDriverCarrierDemo/A4990DualMotorDriverCarrierDemo.ino
@@ -8,18 +8,18 @@
 
 #define LED_PIN 13
 #define POT 14
-//#define LEFTMOTOR1 32
-//#define LEFTMOTOR2 33
-//#define LEFTMOTORITR 34
-//#define RIGHTMOTOR1 1
-//#define RIGHTMOTOR2 3
-//#define RIGHTMOTORITR 35
-#define LEFTMOTOR1 16
-#define LEFTMOTOR2 17
-#define LEFTMOTORITR 33
-#define RIGHTMOTOR1 32
-#define RIGHTMOTOR2 15
-#define RIGHTMOTORITR 27
+#define LEFTMOTOR1 32
+#define LEFTMOTOR2 33
+#define LEFTMOTORITR 34
+#define RIGHTMOTOR1 1
+#define RIGHTMOTOR2 3
+#define RIGHTMOTORITR 35
+//#define LEFTMOTOR1 16
+//#define LEFTMOTOR2 17
+//#define LEFTMOTORITR 33
+//#define RIGHTMOTOR1 32
+//#define RIGHTMOTOR2 15
+//#define RIGHTMOTORITR 27
 
 int D = 0;
 int prevD = 0;
@@ -55,8 +55,9 @@ void setup()
   //**************************************************
 
 
-//D = map(analogRead(POT), 0, 4095, -NOM_PWM_VOLTAGE, NOM_PWM_VOLTAGE);
-D = 0;
+D = map(analogRead(POT), 0, 4095, -NOM_PWM_VOLTAGE, NOM_PWM_VOLTAGE);
+//D = 0;
+
 
 
     //Ensure that you don't go past the maximum possible command
@@ -68,18 +69,20 @@ D = 0;
     }
 
    
-      motors.setSpeeds(-D, D);
+//      motors.setSpeeds(-D, D);
       prevD = D;
+      
 
   motors.stopIfFault();    // It is good practice to stop if there are any errors, stopIfFault() is included for convenience
 }
 
 void loop()
 {
-  motors.setSpeeds(0,0);
+  
+//  motors.setSpeeds(0,0);
   //Stand-in mapping between the pot reading and motor command.
     D = map(analogRead(POT), 0, 4095, -NOM_PWM_VOLTAGE, NOM_PWM_VOLTAGE);
-    D=0;
+   
 
 
     //Ensure that you don't go past the maximum possible command
@@ -91,19 +94,19 @@ void loop()
     }
 
     if (abs(prevD - D) > 5) {
-      motors.setSpeeds(-D, D);
+//      motors.setSpeeds(-D, D);
       prevD = D;
       Serial.println(D);
 //  motors.stopIfFault(); 
     }
 
   
-    if (motors.changeFlagM1 == true) {
-      motors.changeFlagM1 = false;
-//      Serial.println(motors.readM1Rotation());
-    }
-    if (motors.changeFlagM2 == true) {
-      motors.changeFlagM2 = false;
-//      Serial.println(motors.readM2Rotation());
-    }
+//    if (motors.changeFlagM1 == true) {
+//      motors.changeFlagM1 = false;
+////      Serial.println(motors.readM1Rotation());
+//    }
+//    if (motors.changeFlagM2 == true) {
+//      motors.changeFlagM2 = false;
+////      Serial.println(motors.readM2Rotation());
+//    }
 }
